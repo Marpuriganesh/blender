db67d7d38c10927a0a43fd5b85947fa401002e90
marpuri ganesh <marpuriganesh@gmail.com>
2023-04-23 09:19:31 +0530

    added debug line

--- source/blender/nodes/geometry/nodes/node_geo_viewer.cc
+++ source/blender/nodes/geometry/nodes/node_geo_viewer.cc
@@ -32,7 +32,8 @@ static void node_init(bNodeTree * /*tree*/, bNode *node)
   NodeGeometryViewer *data = MEM_cnew<NodeGeometryViewer>(__func__);
   data->data_type = CD_PROP_FLOAT;
   data->domain = ATTR_DOMAIN_AUTO;
-  std::cout<<"this is viewer nodeinitialization\n";
+  std::cout<<"this is viewer node initialization\n";
+  __debugbreak();
   node->storage = data;
 }
 
